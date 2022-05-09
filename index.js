f9e31ab072d0e0fcccc8720b9c056a6468393b61
Valentin <valio31032@gmail.com>
2022-05-09 17:28:13 +0100

    fixed frontend FINALLY

--- index.js
+++ index.js
@@ -1,76 +1,44 @@
 //todo PUSH TO HEROKU!
 
-require('dotenv').config()
 
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
 const Contact = require('./models/contact')
 
-
+const requestLogger = (request, response, next) => {
+	console.log('Method: ', request.method)
+	console.log('Path: ', request.path)
+	console.log('Body: ', request.body)
+	console.log('---')
+	next()
+}
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
+app.use(requestLogger)
 
 app.use(cors())
 
+app.use(morgan('tiny'))
+
 app.use(express.static('build'))
 
-
 app.get('/info', (request, response) => {
-
-	response.setHeader('Content-Type', 'text/html')
-	let contactsInfo = "<p>Phonebook has info for " + contacts.length + " people</p><br/>"
-	let currentDate = new Date()
-	let currentDateStr = "<p>" + currentDate + "</p>"
-	//idk why it has the weird characters in the end...
-	response.write(contactsInfo)
-	response.write(currentDateStr)
-	response.end()
-
-})
-
-app.get('/api/persons/:id', (request, response) => {
-	Contact.findById(request.params.id)
-		.then(contact => {
-			if (contact) {
-				response.json(contact)
-			}
-			else {
-				response.status(404).end()
-			}
-		})
-		.catch(error => next(error))
-})
-
-// const id = Number(request.params.id)
-// console.log('Id received: ', id)
-// const person = contacts.find(p => p.id === id)
-
-// if (person) {
-// 	response.json(person)
-// }
-// else {
-// 	response.status(404).end()
-// }
-
-app.delete('/api/persons/:id', (request, response) => {
-	Contact.findByIdAndRemove(request.params.id)
-		.then(result => {
-			response.status(204).end()
-		})
-		.catch(error => next(error))
+	Contact.find({}).then(contacts => {
+		response.setHeader('Content-Type', 'text/html')
+		let contactsInfo = "<p>Phonebook has info for " + contacts.length + " people</p><br/>"
+		let currentDate = new Date()
+		let currentDateStr = "<p>" + currentDate + "</p>"
+		//idk why it has the weird characters in the end...
+		response.write(contactsInfo)
+		response.write(currentDateStr)
+		response.end()
+	})
 
 })
-
-const generateId = () => {
-	const maxId = contacts.length > 0
-		? Math.max(...contacts.map(n => n.id))
-		: 0
-	return maxId + 1
-}
 
 app.post('/api/persons', (request, response) => {
 	//todo check error in posting somewhere here...part 3c
@@ -84,20 +52,56 @@ app.post('/api/persons', (request, response) => {
 		name: body.name || 'No name in post request gj',
 		number: body.number || 'No number in post request gj',
 	})
-	Contact.find({}).then(contacts => {
-		if (contacts.some(p => p.name === person.name)) {
-			return response.status(400).json({
-				error: 'contact already exists!'
-			})
-		}
-		else {
-			person.save().then(savedPerson => {
-				console.log(savedPerson)
-				response.json(savedPerson)
-			})
-		}
+	person.save().then(savedPerson => {
+		response.json(savedPerson)
 	})
 
+})
+
+app.get('/api/persons', (request, response) => {
+	Contact.find({}).then(contacts => {
+		response.json(contacts)
+	})
+})
+
+
+app.delete('/api/persons/:id', (request, response, next) => {
+	Contact.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+	Contact.findById(request.params.id)
+		.then(contact => {
+			if (contact) {
+				response.json(contact)
+			}
+			else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
+
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+	const body = request.body
+
+	const contact = {
+		name: body.name,
+		number: body.number,
+	}
+
+	Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+		.then(updatedContact => {
+			console.log('updated contact????: ', updatedContact)
+			console.log('how tf is the list looking?: ', Contact.find({}))
+			response.json(updatedContact)
+		})
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -108,21 +112,14 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
 	console.error(error.message)
-
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' })
 	}
-
 	next(error)
 }
 
 app.use(errorHandler)
 
-app.get('/api/persons', (request, response) => {
-	Contact.find({}).then(contacts => {
-		response.json(contacts)
-	})
-})
 
 const PORT = process.env.PORT
 app.listen(PORT)
